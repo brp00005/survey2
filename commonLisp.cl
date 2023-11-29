e48569673a000a1877e51375bc6ec5b584a92df3
brp00005 <brp00005@mix.wvu.edu>
2023-11-29 11:31:06 -0500

    update headers

---
+++ commonLisp.cl
@@ -0,0 +1,2 @@
+;Author: Robert Coleman, coauthor: Branden Purdum
+;Statement: This code follows the academic integrity policy
--- scala.scala
+++ scala.scala
@@ -1,47 +1,41 @@
-//Authors: Branden Purdum and Robert Coleman
+//Authors: Robert Coleman and Branden Purdum
+//Statement: This code follows the academic integrity policy
 
-//import unit test stuff
-import org.scalatest.funspec.AnyFunSpec
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.funsuite.AnyFunSuite
 
-class scala extends AnyFunSpec{
-  //unit tests
-  describe("same"){
-
-    //unit test 1
-    it("recursively compare [1, 2, 8], [1, 2, 8]"){
-      val result = same([1, 2, 8], [1, 2, 8])
-      result shouldEqual true
-    }
-    //unit test 2
-    it("recursively compare [1, 2, 2], [1, 2, 8]"){
-      val result = same([1, 2, 2], [1, 2, 8])
-      result shouldEqual false
-    }
-
-    //unit test 3, null numbers edge case
-    it("recursively compare [], [1, 2, 8]"){
-      val result = same([], [1, 2, 8])
-      result shouldEqual false
-    }
-
-    //unit test 4, null null edge case
-    it("recursively compare [], []"){
-      val result = same([], [])
-      result shouldEqual false
-    }
-  }
-
-
-  //function definition and whatnot
-  def same(list1: List[int], list2: List[int]): Boolean{
-    //base cases: null null list, then only 1 null List
-
-
-    //recursive call, if one is not equal, false 
-
+//make stuff for tests 
+object TestRunner {
+  def main(args: Array[String]): Unit = {
+    
+    //instantiate object and run tests 
+    val testSuite = new TestSuite
+    testSuite.execute()
   }
 }
 
-  
-             
+//same function and unit tests
+class TestSuite extends AnyFunSuite {
+  def same[A](list1: List[A], list2: List[A]): Boolean = {
+    (list1, list2) match {
+      case (Nil, Nil) => true
+      case (Nil, _) => false
+      case (_, Nil) => false
+      case (head1 :: tail1, head2 :: tail2) if head1 == head2 =>
+        same(tail1, tail2)
+      case _ => false
+    }
+  }
+
+  test("Test edge cases: ") {
+    assert(same(List(), List(1, 2, 3)) == false)
+    assert(same(List(1, 2, 3), List()) == false)
+    assert(same(List(), List()) == true)
+  }
+
+  test("Test non-trivial cases: ") {
+    assert(same(List(1,2,4), List(1, 2, 3)) == false)
+    assert(same(List(1, 2, 3), List(1,2,3)) == true)
+    assert(same(List(3,2,1,5), List(3,2,1,1)) == false)
+    assert(same(List(3,2,1,5), List(3,2,1,5)) == true)
+  }
+}
